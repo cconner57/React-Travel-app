941c9584306f40931fc6d3af0b4eaa1e1f18a1af
Chris Conner <chris.conner57@gmail.com>
2020-11-01 16:20:21 -0800

    Removed redirect, changed video path

--- client/src/Home/Main.js
+++ client/src/Home/Main.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Guide from './Guide';
+import video from './background-video.mp4';
 import './Home.scss';
 
 const Main = () => {
@@ -23,8 +24,7 @@ const Main = () => {
 
 	useEffect(() => {
 		const text = ['Travel', ' Plan', ' Explore'];
-		const interval = 
-			setTimeout(() => {
+		const interval = setTimeout(() => {
 			text.forEach((word, i) => {
 				setTimeout(() => {
 					setContent((prevWord) => [...prevWord, word]);
@@ -32,20 +32,30 @@ const Main = () => {
 			});
 		}, 1500);
 		return () => {
-			clearTimeout(interval)
-		}
+			clearTimeout(interval);
+		};
 	}, []);
 
 	return (
 		<>
 			<div className='Landing'>
-				<video className='Landing__Background' loop muted autoPlay>
+				{/* <video className='Landing__Background' loop muted autoPlay>
 					<source src='./Images/Home/background-video.mp4' type='video/mp4' />
-				</video>
+				</video> */}
+				<video
+					className='Landing__Background'
+					src={video}
+					loop
+					muted
+					autoPlay></video>
 				<h5 className='Landing__Caption'>{content}</h5>
 				<div className={`Landing__Box ${hide && box.classList.add('HideBox')}`}>
 					<p>See More</p>
-					<img className='Arrow' src='./Images/Home/down-arrow.png' alt='See More' />
+					<img
+						className='Arrow'
+						src='./Images/Home/down-arrow.png'
+						alt='See More'
+					/>
 				</div>
 			</div>
 			<Guide />
